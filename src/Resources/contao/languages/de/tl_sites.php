702510d7e35ba175146719e1b31d76e4c6d45169
lautschrift <steffen@lautschrift.com>
2020-07-31 19:53:01 +0200

    up

---
+++ src/Resources/contao/languages/de/tl_sites.php
@@ -0,0 +1,3 @@
+$GLOBALS['TL_LANG']['tl_site']['new'] = ['Neue Fundstelle', 'Erzeugt eine neue Fundstelle.'];
+$GLOBALS['TL_LANG']['tl_site']['site_legend'] = 'Informationen zur Fundstelle';
+$GLOBALS['TL_LANG']['tl_site']['name'] = ['Name', 'Name der Fundstelle'];
