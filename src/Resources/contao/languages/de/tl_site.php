6dbe5bac72479dd6021d296565d9064e38838f2d
lautschrift <steffen@lautschrift.com>
2020-08-01 02:04:41 +0200

    Update tl_site.php

--- src/Resources/contao/languages/de/tl_site.php
+++ src/Resources/contao/languages/de/tl_site.php
@@ -3,5 +3,5 @@ $GLOBALS['TL_LANG']['tl_site']['site_legend'] = 'Informationen zur Fundstelle';
 $GLOBALS['TL_LANG']['tl_site']['details_legend'] = 'Informationen zur Fundstelle';
 
 $GLOBALS['TL_LANG']['tl_site']['name'] = ['Name', 'Name der Fundstelle'];
-$GLOBALS['TL_LANG']['tl_site']['site_country'] = ['Land', 'Land der Fundstelle'];
-$GLOBALS['TL_LANG']['tl_site']['unescoid'] = ['UNESCO KENNUNG', 'Kennung der UNESCO'];
+$GLOBALS['TL_LANG']['tl_site']['country'] = ['Land', 'Land der Fundstelle'];
+$GLOBALS['TL_LANG']['tl_site']['unescoid'] = ['UNESCO Kennung', 'Kennung der UNESCO'];
