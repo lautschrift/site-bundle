7e6b22c183b72be01f7f6f5cdad4752d0f436b3d
lautschrift <steffen@lautschrift.com>
2020-08-01 15:50:12 +0200

    icon

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -36,13 +36,18 @@ array_insert($GLOBALS['TL_CTE']['Museum'] ,1, array(
 		'museum' => 'Lautschrift\\MuseumBundle\\Resources\\contao\\classes\\ContentMuseum'
 ));
 */
+
+if(TL_MODE == "BE") {
+    $GLOBALS['TL_CSS'][] = '/bundles/site-bundle/palafittes.css';
+}
+
 array_insert($GLOBALS['BE_MOD'], array_search('content', array_keys($GLOBALS['BE_MOD'])) + 1, array('Site' => array()));
 array_insert($GLOBALS['BE_MOD']['Site'],1, array
 (
         'palafittes_site' => array
         (
             'tables' => array ('tl_site', 'tl_site_details'),
-            'icon' => 'bundles/con4gismaps/images/be-icons/baselayers.png',
+            'icon' => 'bundles/con4gismaps/images/be-icons/palafittes.svg',
         ),
 ));
 
