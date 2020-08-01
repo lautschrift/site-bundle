5ca78da7d932e1ec7edbe74f27dc542a183e3dd2
lautschrift <steffen@lautschrift.com>
2020-08-01 14:35:46 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -36,10 +36,10 @@ array_insert($GLOBALS['TL_CTE']['Museum'] ,1, array(
 		'museum' => 'Lautschrift\\MuseumBundle\\Resources\\contao\\classes\\ContentMuseum'
 ));
 */
-
+array_insert($GLOBALS['BE_MOD'], array_search('content', array_keys($GLOBALS['BE_MOD'])) + 1, array('Site' => array()));
 array_insert($GLOBALS['BE_MOD']['Site'],1, array
 (
-        'site' => array
+        'palafittes_site' => array
         (
             'tables' => array ('tl_site', 'tl_site_details'),
         ),
