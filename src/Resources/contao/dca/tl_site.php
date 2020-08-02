28b4af5248f9a6f6c552dab7c4d6d50d78db7989
lautschrift <steffen@lautschrift.com>
2020-08-02 21:18:09 +0200

    Update tl_site.php

--- src/Resources/contao/dca/tl_site.php
+++ src/Resources/contao/dca/tl_site.php
@@ -161,7 +161,7 @@ $GLOBALS['TL_LANG']['site_country']['wert6'] = 'Frankreich';
  * Class t tl_site_c4g_maps_site
  */
 
-class tl_site_c4g_maps_site extends Backend
+class tl_site_c4g_maps_site extends \Backend
 {
 
     protected $firstMapId = null;
