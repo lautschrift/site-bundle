2cf44bd07c704dc8a0f52e24dc36a5fe5c4350d2
lautschrift <steffen@lautschrift.com>
2020-08-02 19:33:44 +0200

    .

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,40 +3,6 @@
  * Back end modules
 */
 
-/*
-array_insert($GLOBALS['BE_MOD']['content'], 1, array
-(
-	'Museum' => array
-	(
-		'tables'	=> array('tl_museum'),
-
-	)
-));
-*/
-/**
- * Content elements
-*/
-/*
-array_insert($GLOBALS['TL_CTE']['Museum'] ,1, array(
-		'museum' => 'Lautschrift\\Museum-Bundle\\ContentMuseum'
-));
-
-*/
-
-/*
-
-$GLOBALS['TL_CTE']['Museum'] = array
-(
-	'museum' => 'MuseumBundle\Resources\ContentMuseum',
-
-);
-
-
-array_insert($GLOBALS['TL_CTE']['Museum'] ,1, array(
-		'museum' => 'Lautschrift\\MuseumBundle\\Resources\\contao\\classes\\ContentMuseum'
-));
-*/
-
 if(TL_MODE == "BE") {
     $GLOBALS['TL_CSS'][] = '/bundles/site/palafittes.css';
 }
