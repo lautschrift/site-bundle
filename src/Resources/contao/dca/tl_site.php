d5a674292852f329754d4a9b8334da367eb05008
lautschrift <steffen@lautschrift.com>
2020-08-02 20:58:23 +0200

    Update tl_site.php

--- src/Resources/contao/dca/tl_site.php
+++ src/Resources/contao/dca/tl_site.php
@@ -115,7 +115,7 @@ $GLOBALS['TL_LANG']['site_country']['wert6'] = 'Frankreich';
             'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
             'inputType'               => 'c4g_text',
             'save_callback'           => [['tl_site_c4g_maps_site', 'setLocLon']],
-            'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoPicker', 'getPickerLink']],
+            'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoEditor', 'getEditorLink']],
             'sql' => ['type' => 'string', 'length' => 20, 'default' => '']
         ],
         'geoY' => [
@@ -123,7 +123,7 @@ $GLOBALS['TL_LANG']['site_country']['wert6'] = 'Frankreich';
             'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
             'inputType'               => 'c4g_text',
             'save_callback'           => [['tl_site_c4g_maps_site', 'setLocLat']],
-            'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoPicker', 'getPickerLink']],
+            'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoEditor', 'getEditorLink']],
             'sql' => ['type' => 'string', 'length' => 20, 'default' => '']
         ],
         'singleSRC' => [
