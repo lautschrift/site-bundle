4e5e43dd0c853504fdd6ed0e174868b629a45e81
lautschrift <steffen@lautschrift.com>
2020-08-02 15:32:55 +0200

    Update tl_site.php

--- src/Resources/contao/languages/de/tl_site.php
+++ src/Resources/contao/languages/de/tl_site.php
@@ -6,5 +6,5 @@ $GLOBALS['TL_LANG']['tl_site']['name'] = ['Name', 'Name der Fundstelle'];
 $GLOBALS['TL_LANG']['tl_site']['country'] = ['Land', 'Land der Fundstelle'];
 $GLOBALS['TL_LANG']['tl_site']['unescoid'] = ['UNESCO Kennung', 'Kennung der UNESCO'];
 $GLOBALS['TL_LANG']['tl_site']['region'] = ['Region', 'Region der Fundstelle'];
-$GLOBALS['TL_LANG']['tl_site']['geoX'] = ['Geokoordinate X', 'WGS84 Geokoordinate der Fundstelle'];
-$GLOBALS['TL_LANG']['tl_site']['geoY'] = ['Geokoordinate Y, 'WGS84 Geokoordinate der Fundstelle'];
+$GLOBALS['TL_LANG']['tl_site']['geoX'] = ['Geokoordinate E', 'WGS84 E - Geokoordinate der Fundstelle'];
+$GLOBALS['TL_LANG']['tl_site']['geoY'] = ['Geokoordinate N', 'WGS84 N - Geokoordinate der Fundstelle'];
