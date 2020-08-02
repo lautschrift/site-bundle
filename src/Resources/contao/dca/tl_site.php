f38f70cd9736cb337be118d1f17d28fd12152989
lautschrift <steffen@lautschrift.com>
2020-08-02 17:41:10 +0200

    Update tl_site.php

--- src/Resources/contao/dca/tl_site.php
+++ src/Resources/contao/dca/tl_site.php
@@ -144,7 +144,8 @@ $GLOBALS['TL_LANG']['site_country']['wert6'] = 'Frankreich';
  * Class t tl_site_c4g_maps_site
  */
 
-use con4gis\MapsBundle\Classes\Utils;
+
+use \con4gis\MapsBundle\Resources\contao\classes\Utils
 
 class tl_site_c4g_maps_site extends Backend
 {
