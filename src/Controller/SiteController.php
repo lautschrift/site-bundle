3a8e66aa3f3612883fe727aa3230e665e8ea3ce4
lautschrift <steffen@lautschrift.com>
2020-08-03 18:21:23 +0200

    Update SiteController.php

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -1,13 +1,30 @@
 <?php
 
-namespace Lautschrift\SiteBundle\Controller;
+//namespace Lautschrift\SiteBundle\Controller;
+namespace App\Controller;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
+use Contao\ModuleModel;
+use Contao\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @FrontendModule(category="miscellaneous")
  */
 class SiteController extends AbstractFrontendModuleController
 {
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
+       {
+           if ($request->isMethod('post')) {
+              /* if (null !== ($redirectPage = PageModel::findByPk($model->jumpTo))) {
+                   throw new RedirectResponseException($redirectPage->getAbsoluteUrl());
+               }*/
+           }
 
+           $template->unescoid = $model->unescoid;
+           return $template->getResponse();
+
+       }
 }
