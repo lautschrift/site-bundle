41f54b606289553decf56ef3ba5690a7a2b89c0d
lautschrift <steffen@lautschrift.com>
2020-08-03 11:06:02 +0200

    Update tl_site_details.php

--- src/Resources/contao/dca/tl_site_details.php
+++ src/Resources/contao/dca/tl_site_details.php
@@ -67,8 +67,8 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_site_details']['type'],
                 'inputType' => 'select',
                 'eval' 		=> array('submitOnChange' => true, 'tl_class' => 'clr', 'mandatory' => true),
-        		'options' 	=> ['default','text', 'image'],
-        		//'reference' => &$GLOBALS['TL_LANG']['site_country'],
+        		'options' 	=> ['text', 'image'],
+        		'reference' => &$GLOBALS['TL_LANG']['site_country'],
                 'sql' => ['type' => 'string', 'length' => 20, 'default' => '']
             ],
             'name' => [
@@ -108,7 +108,7 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
         ],
         'palettes' => [
            '__selector__' => ['type'],
-           'default' => '{default_legend},name',
+
            'text' => '{title_legend},name,description',
            'image'=> '{image_legend},singleSRC, name',
        ],
