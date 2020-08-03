13f17d755b787875eec2e7e62f6b4bac7a71a344
lautschrift <steffen@lautschrift.com>
2020-08-03 13:38:03 +0200

    Create tl_site_details.php

---
+++ src/Resources/contao/languages/de/tl_site_details.php
@@ -0,0 +1,9 @@
+$GLOBALS['TL_LANG']['tl_site_details']['new'] = ['Neuer Eintrag', 'Erzeugt eine neue Detailseite oder einen Audioguide zur Fundstelle.'];
+$GLOBALS['TL_LANG']['tl_site_details']['type_legend'] = 'Typ des Eintrags';
+$GLOBALS['TL_LANG']['tl_site_details']['details_legend'] = 'Details';
+
+$GLOBALS['TL_LANG']['tl_site_details']['tl_site']['type'] = ['Typ', 'Typ des Eintrags'];
+$GLOBALS['TL_LANG']['tl_site_details']['tl_site']['speech'] = ['Sprache', 'Erfassungssprache des Eintrags'];
+$GLOBALS['TL_LANG']['tl_site_details']['tl_site']['datation'] = ['Datierung', 'Angabe der Datierung in Zahlen'];
+$GLOBALS['TL_LANG']['tl_site_details']['tl_site']['genericdatation'] = ['Zeitalter', 'Angabe der Datierung in Zeitaltern'];
+$GLOBALS['TL_LANG']['tl_site_details']['tl_site']['description'] = ['Beschreibung', 'Kurzbeschreibung der Fundstelle (350 - 600 Zeichen)'];
