6b65fcaebaed00d4c806ce68abfc57de1739a739
lautschrift <steffen@lautschrift.com>
2020-08-03 18:12:29 +0200

    .

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lautschrift\SiteBundle\Controller;
+
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+
+/**
+ * @FrontendModule(category="miscellaneous")
+ */
+class SiteController extends AbstractFrontendModuleController
+{
+
+}
