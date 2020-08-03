5d9c0608f3cb5158831b732cdd5984d58d59865d
lautschrift <steffen@lautschrift.com>
2020-08-03 19:38:48 +0200

    .

---
+++ src/Controller/FrontendModule/SiteModule.php
@@ -0,0 +1,26 @@
+<?php
+
+//namespace Lautschrift\SiteBundle\Controller;
+namespace App\Controller\FrontendModule;
+
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
+use Contao\ModuleModel;
+use Contao\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @FrontendModule("my_example",
+ *   category="miscellaneous",
+ *   template="mod_site",
+ *   renderer="forward"
+ * )
+ */
+class SiteModule extends AbstractFrontendModuleController
+{
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
+  {
+      return $template->getResponse();
+  }
+}
