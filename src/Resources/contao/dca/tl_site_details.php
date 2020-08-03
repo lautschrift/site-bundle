7f1619a7899c743bd86c7701c7385139a89ac170
lautschrift <steffen@lautschrift.com>
2020-08-03 14:14:28 +0200

    Update tl_site_details.php

--- src/Resources/contao/dca/tl_site_details.php
+++ src/Resources/contao/dca/tl_site_details.php
@@ -1,6 +1,6 @@
 
 <?php
-
+$GLOBALS['TL_LANG']['site_details_type']['wert0'] = '- Typ auswählen –';
 $GLOBALS['TL_LANG']['site_details_type']['wert1'] = 'Fundstellen Details';
 $GLOBALS['TL_LANG']['site_details_type']['wert2'] = 'Audio Fundstelle';
 
@@ -24,7 +24,7 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
             function (\Contao\DataContainer $dc) {
                 $db = \Contao\Database::getInstance();
                 $pid = \Contao\Input::get('pid');
-                $result = $db->prepare('SELECT `name` FROM `tl_site` WHERE `id` = ?')
+                $result = $db->prepare('SELECT `unescoid` FROM `tl_site` WHERE `id` = ?')
                              ->execute([$pid]);
                 $prefix = strtoupper(substr($result->name, 0, 2));
                 $GLOBALS['TL_DCA']['tl_site_details']['fields']['number']['default'] = $prefix;
@@ -35,8 +35,8 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
     'list' => [
         'sorting' => [
             'mode' => 4,
-            'fields' => ['speech'],
-            'headerFields' => ['speech'],
+            'fields' => ['number'],
+            'headerFields' => ['name'],
             'panelLayout' => 'search,limit',
             'child_record_callback' => function (array $row) {
                 return '<div class="tl_content_left">'.$row['name'].' ['.$row['number'].']</div>';
@@ -78,7 +78,7 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_site']['type'],
                 'inputType' => 'select',
                 'eval' 		=> array('submitOnChange' => true, 'tl_class' => 'clr', 'mandatory' => true),
-        		'options' 	=> ['wert1', 'wert2'],
+        		'options' 	=> ['wert0','wert1', 'wert2'],
         		'reference' => &$GLOBALS['TL_LANG']['site_details_type'],
                 'sql' => ['type' => 'string', 'length' => 20, 'default' => 0]
             ],
@@ -95,7 +95,7 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
                 'search' => true,
                 'flag' => 1,
                 'inputType' => 'text',
-                'eval' => ['tl_class' => 'w50', 'maxlength' => 255, 'mandatory' => true],
+                'eval' => ['tl_class' => 'w50', 'maxlength' => 255, 'mandatory' => false],
                 'sql' => ['type' => 'string', 'length' => 255, 'default' => '']
             ],
             'genericdatation' => [
@@ -103,28 +103,28 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
                 'search' => true,
                 'flag' => 1,
                 'inputType' => 'text',
-                'eval' => ['tl_class' => 'w50', 'maxlength' => 255, 'mandatory' => true],
+                'eval' => ['tl_class' => 'w50', 'maxlength' => 255, 'mandatory' => false],
                 'sql' => ['type' => 'string', 'length' => 255, 'default' => '']
             ],
             'activities' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_site_details']['activities'],
                 'search' => true,
                 'inputType' => 'textarea',
-                'eval' => ['tl_class' => 'clr', 'rte' => 'tinyMCE', 'mandatory' => true],
+                'eval' => ['tl_class' => 'clr', 'rte' => 'tinyMCE', 'mandatory' => false],
                 'sql' => ['type' => 'text', 'notnull' => false]
             ],
             'features' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_site_details']['features'],
                 'search' => true,
                 'inputType' => 'textarea',
-                'eval' => ['tl_class' => 'clr', 'rte' => 'tinyMCE', 'mandatory' => true],
+                'eval' => ['tl_class' => 'clr', 'rte' => 'tinyMCE', 'mandatory' => false],
                 'sql' => ['type' => 'text', 'notnull' => false]
             ],
             'museumlink' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_site_details']['features'],
                 'search' => true,
                 'inputType' => 'text',
-                'eval' => ['tl_class' => 'clr', 'rte' => 'tinyMCE', 'mandatory' => true],
+                'eval' => ['tl_class' => 'clr', 'rte' => 'tinyMCE', 'mandatory' => false],
                 'sql' => ['type' => 'text', 'notnull' => false]
             ],
             'name' => [
