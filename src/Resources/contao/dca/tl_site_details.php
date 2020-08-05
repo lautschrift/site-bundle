fdfd1fce538ed09f685c2c2f771a574d0bd96b47
lautschrift <steffen@lautschrift.com>
2020-08-05 16:57:15 +0200

    Update tl_site_details.php

--- src/Resources/contao/dca/tl_site_details.php
+++ src/Resources/contao/dca/tl_site_details.php
@@ -28,7 +28,9 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
                              ->execute([$pid]);
                 $prefix = $result->unescoid; //strtoupper(substr($result->name, 0, 2));
                 $GLOBALS['TL_DCA']['tl_site_details']['fields']['number']['default'] = $prefix;
-
+                $id = 'myID';
+                $setChildToParent = $db->prepare('UPDATE `tl_site` SET `details_link` = ? WHERE `id` = ?')
+                                        ->execute([$id, $pid]);
             },
         ],
         'onsubmit_callback' => [
