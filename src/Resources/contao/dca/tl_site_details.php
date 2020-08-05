6b465da8553e081c4e4013448bbfdc8021e11495
lautschrift <steffen@lautschrift.com>
2020-08-05 17:09:31 +0200

    Update tl_site_details.php

--- src/Resources/contao/dca/tl_site_details.php
+++ src/Resources/contao/dca/tl_site_details.php
@@ -44,7 +44,7 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
                 $db = \Contao\Database::getInstance();
                 $pid = \Contao\Input::get('pid');
                 $id = '';
-                $removeChildId => $db->prepare('UPDATE `tl_site` SET `details_link` = ? WHERE `id` = ?')
+                $removeChildId = $db->prepare('UPDATE `tl_site` SET `details_link` = ? WHERE `id` = ?')
                                         ->execute([$id, $pid]);
             },
         ],
