4d9cfef9ea9cf08d39ea85cb5b5c3b44e606ce83
lautschrift <steffen@lautschrift.com>
2020-08-06 17:25:31 +0200

    Update SiteModuleController.php

--- src/Controller/FrontendModule/SiteModuleController.php
+++ src/Controller/FrontendModule/SiteModuleController.php
@@ -32,7 +32,7 @@ class SiteModuleController extends AbstractFrontendModuleController
          $resultSite = $db->prepare('SELECT * FROM `tl_site` WHERE `id`= ?')
                ->execute([$detailParent])->fetchAllAssoc();
          $template->detailParent = $detailParent;
-         $template->sitedetails = $resultDetails->fetchAllAssoc();
+         $template->sitedetails = $sitedetails[0] = $resultDetails->fetchAllAssoc();
          $template->site = $resultSite[0];
 
          return $template->getResponse();
