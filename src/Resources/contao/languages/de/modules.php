b440455503cd619cf3949a5619a7295b739d7982
lautschrift <steffen@lautschrift.com>
2020-08-06 13:24:45 +0200

    Update modules.php

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,4 +1,4 @@
 $GLOBALS['TL_LANG']['MOD']['Site'] = ['Palafittes', 'Fundstellen verwalten.'];
 $GLOBALS['TL_LANG']['MOD']['tl_site'] = 'Fundstellen';
-$GLOBALS['TL_LANG']['MOD']['site_module'] = 'Fundstelle';
+$GLOBALS['TL_LANG']['MOD']['site_module'] = ['Fundstelle', 'Ausgabe der Fundestellendetails';
 $GLOBALS['TL_LANG']['MOD']['palafittes_site'] = 'Fundstellen';
