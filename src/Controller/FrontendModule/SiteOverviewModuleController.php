bbdfe264265769e99c46ad768516eb446f596a8d
lautschrift <steffen@lautschrift.com>
2020-08-07 09:37:55 +0200

    overview

---
+++ src/Controller/FrontendModule/SiteOverviewModuleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lautschrift\SiteBundle\Controller\FrontendModule;
+
+use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
+use Contao\ModuleModel;
+use Contao\Template;
+use Contao\Database;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @FrontendModule("siteoverview_module",
+ *   category="Fundstellen",
+ *   template="mod_siteoverview",
+ * )
+ */
+class SiteOverviewModuleController extends AbstractFrontendModuleController
+{
+    protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
+      {
+         global $objPage;
+         $myID = \Contao\Input::get('id');
+         $db = \Contao\Database::getInstance();
+         $result = $db->prepare('SELECT * FROM `tl_site`')
+            ->execute();
+
+         $sites = $result->fetchAllAssoc();
+         $template->sitedes = $sites[0];
+
+         return $template->getResponse();
+      }
+}
--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -3,3 +3,4 @@ $GLOBALS['TL_LANG']['MOD']['tl_site'] = 'Fundstellen';
 $GLOBALS['TL_LANG']['MOD']['palafittes_site'] = 'Fundstellen';
 
 $GLOBALS['TL_LANG']['FMD']['site_module'] = ['Fundstelle', 'Ausgabe der Fundestellendetails'];
+$GLOBALS['TL_LANG']['FMD']['siteoverview_module'] = ['Fundstellenübersicht', 'Ausgabe der Fundestellenübersicht'];
