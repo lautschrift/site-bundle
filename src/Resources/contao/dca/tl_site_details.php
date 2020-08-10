fb76e0ad7d2c8aca0bac2a29201562b29d5b56a1
lautschrift <steffen@lautschrift.com>
2020-08-10 21:29:23 +0200

    Update tl_site_details.php

--- src/Resources/contao/dca/tl_site_details.php
+++ src/Resources/contao/dca/tl_site_details.php
@@ -351,7 +351,7 @@ class tl_site_details extends Backend
 
        if(!in_array($locatedLink, $allIds)) {
            $allIds[] = $locatedLink;
-           //$allIdsAsString = json_encode($allIds);
+           $allIdsAsString = json_encode($allIds);
            $setChildToParent = $this->Database->prepare('UPDATE `tl_site` SET `details_link` = ? WHERE `id` = ?')
                                    ->execute([$allIds, $pid]);
        }
