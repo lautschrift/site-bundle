7bbb2381bf829f73c205b532dcc4c2da613a238d
lautschrift <steffen@lautschrift.com>
2020-08-14 12:18:41 +0200

    Update tl_site_details.php

--- src/Resources/contao/dca/tl_site_details.php
+++ src/Resources/contao/dca/tl_site_details.php
@@ -167,7 +167,7 @@ $GLOBALS['TL_DCA']['tl_site_details'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_site']['speech'],
                 'inputType' => 'select',
                 'eval' 		=> array('submitOnChange' => true, 'tl_class' => 'clr', 'mandatory' => true),
-        		'options' 	=> ['DE', 'EN', 'FR', 'SLO'],
+        		'options' 	=> ['DE', 'EN', 'FR', 'SLO', 'IT'],
         		'reference' => &$GLOBALS['TL_LANG']['site_details_speech'],
                 'sql' => ['type' => 'string', 'length' => 3, 'default' => 0]
             ],
