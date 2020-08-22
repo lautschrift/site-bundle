9ca162c8a3e2b96c35764cf784b02c7ae691a986
lautschrift <steffen@lautschrift.com>
2020-08-22 14:33:12 +0200

    Update SiteModuleController.php

--- src/Controller/FrontendModule/SiteModuleController.php
+++ src/Controller/FrontendModule/SiteModuleController.php
@@ -28,13 +28,34 @@ class SiteModuleController extends AbstractFrontendModuleController
             ->execute([$myID]);
 
          $detailParent = $resultDetails->pid;
+         $detailSpeech = $resultDetails->speech;
 
-         $resultSite = $db->prepare('SELECT * FROM `tl_site` WHERE `id`= ?')
-               ->execute([$detailParent])->fetchAllAssoc();
-         $template->detailParent = $detailParent;
-         $sitedetails = $resultDetails->fetchAllAssoc();
-         $template->sitedetails = $sitedetails[0];
-         $template->site = $resultSite[0];
+         if (strtoupper($objPage->language) !== $resultDetails->speech) {
+             $storedLinks = $db->prepare('SELECT `details_link` FROM `tl_site` WHERE `id`= ?')
+                   ->execute([$detailParent])->fetchAllAssoc();
+             $allLinks = json_decode($storedLinks->details_link, true);
+             foreach ($allLinks as $key => $val) {
+                 $tmp = explode(";",$val);
+                 if($tmp[1] === strtoupper($objPage->language)) {
+                     $resultSite = $db->prepare('SELECT * FROM `tl_site` WHERE `id`= ?')
+                           ->execute([$tmp[0])->fetchAllAssoc();
+                     $template->detailParent = $detailParent;
+                     $sitedetails = $resultDetails->fetchAllAssoc();
+                     $template->sitedetails = $sitedetails[0];
+                     $template->site = $resultSite[0];
+                 }
+            }
+         } else {
+             $resultSite = $db->prepare('SELECT * FROM `tl_site` WHERE `id`= ?')
+                   ->execute([$detailParent])->fetchAllAssoc();
+             $template->detailParent = $detailParent;
+             $sitedetails = $resultDetails->fetchAllAssoc();
+             $template->sitedetails = $sitedetails[0];
+             $template->site = $resultSite[0];
+         }
+
+
+
 
          return $template->getResponse();
       }
