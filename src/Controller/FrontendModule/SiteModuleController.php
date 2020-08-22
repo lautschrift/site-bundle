46aeffaaad72fbfb2ee022eb203dd0225af015eb
lautschrift <steffen@lautschrift.com>
2020-08-22 15:43:11 +0200

    Update SiteModuleController.php

--- src/Controller/FrontendModule/SiteModuleController.php
+++ src/Controller/FrontendModule/SiteModuleController.php
@@ -47,10 +47,13 @@ class SiteModuleController extends AbstractFrontendModuleController
                  if($tmp[1] === strtoupper($objPage->language || $tmp[1] === 'EN')) {
                      $resultSite = $db->prepare('SELECT * FROM `tl_site` WHERE `id`= ?')
                            ->execute([$tmp[0]])->fetchAllAssoc();
+                           $temmplate->resu = 'in';
                      $template->detailParent = $detailParent;
                      $sitedetails = $resultDetails->fetchAllAssoc();
                      $template->sitedetails = $sitedetails[0];
                      $template->site = $resultSite[0];
+                 } else {
+                     $temmplate->resu = 'out';
                  }
             }
          } else {
