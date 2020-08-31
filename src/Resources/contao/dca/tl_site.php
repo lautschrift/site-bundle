7580ea89018c834458da276dc91ab734a20b39c5
lautschrift <steffen@lautschrift.com>
2020-08-31 22:37:06 +0200

    Update tl_site.php

--- src/Resources/contao/dca/tl_site.php
+++ src/Resources/contao/dca/tl_site.php
@@ -4,8 +4,10 @@
 
 //use con4gis\MapsBundle\classes\GeoPicker;
 //use con4gis\MapsBundle\classes\Utils;
-use con4gis\MapsBundle\Resources\contao\classes\GeoPicker;
+
+use \con4gis\MapsBundle\Resources\contao\classes\GeoPicker;
 use con4gis\MapsBundle\Resources\contao\classes\Utils;
+
 // Controller::loadDataContainer('tl_content');
 
 /**
